c5d58fe92a7b542f6e76b90289427ba12e03de63
Mehrdad Rashidi <rashidi.mehrdad@gmail.com>
2022-12-05 17:30:53 +0330

    Forth Commit

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list_notes())
+    path('notes', views.list_notes)
 ]
--- notes/views.py
+++ notes/views.py
@@ -5,4 +5,4 @@ from .models import Notes
 # Create your views here.
 def list_notes(request):
     all_notes = Notes.objects.all()
-    return render(request, 'notes/notesList.html', {'notes': all_notes})
+    return render(request, 'notes/notes_List.html', {'notes': all_notes})
